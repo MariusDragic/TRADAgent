e2b92afbb7adfb095ec8bc2bdc9e3ab2d2a4274b
Marius Dragic <marius.dragic@gmail.com>
2026-01-12 18:56:35 +0100

    tradagent package

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+from langchain.messages import HumanMessage
+
+from tradagent.agents.stock_analyst import build_agent
+from tradagent.utils.answer_utils import extract_final_answer
+
+
+
 def main():
-    print("Hello from tradagent!")
+    agent = build_agent()
+
+    print("Financial Agent (Mistral Medium)")
+    print("Type 'exit' to quit.\n")
+
+    while True:
+        user_input = input("CHAT > ")
+
+        if user_input.lower() in {"exit", "quit"}:
+            break
+
+        response = agent.invoke(
+            {
+                "messages": [
+                    HumanMessage(content=user_input)
+                ]
+            }
+        )
+        response = extract_final_answer(response)
+        print("\nBOT >", response, "\n")
 
 
 if __name__ == "__main__":
